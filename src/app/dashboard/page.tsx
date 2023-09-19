cfbaa2fb25ad87caedb76744b26c7cc2dde12ec5
Obada AbdAlkhaleq | عبادة عبد الخالق <obada.abdalkhaleq@gmail.com>
2023-09-19 15:26:49 +0300

    QuizCard and HistoryCard for dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { getSession } from "@/lib/nextauth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import QuizCard from "@/components/dashboard/QuizCard";
+import HistoryCard from "@/components/dashboard/HistoryCard";
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+};
+
+const DashboardPage = async () => {
+  const session = await getSession();
+  if (!session?.user) {
+    return redirect('/');
+  }
+  return (
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="text-3xl font-bold mr-2 tracking-tight">Dashboard</h2>
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7"></div>
+    </main>
+  );
+};
+
+export default DashboardPage;
