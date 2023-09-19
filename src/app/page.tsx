ac29006c9d42458e47274f1d36316912423ae2f6
Obada AbdAlkhaleq | عبادة عبد الخالق <obada.abdalkhaleq@gmail.com>
2023-09-19 14:18:39 +0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import SignInButton from "@/components/SignInButton";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { getSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+
+export default async function Home() {
+  const session = await getSession();
+  if (session?.user) {
+    return redirect('/dashboard');
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button type="submit">Hello world</Button>
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Quizify🙇‍♂️!</CardTitle>
+          <CardDescription>Quizify is a platform for creating quizzes using AI! Get Started by logging in</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton
+            text="Sign In with Google"
+          ></SignInButton>
+        </CardContent>
+      </Card>
     </main>
   );
 }
