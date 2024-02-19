6f686f13e00d1deaf7c031ad198609b09fea4c7f
Obada AbdAlkhaleq | عبادة عبد الخالق <obada.abdalkhaleq@gmail.com>
2024-02-19 22:11:50 +0200

    refactor: signinbutton formatting

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,17 +1,21 @@
-'use client';
+"use client";
 import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
 
 type Props = {
-	text: string;
+  text: string;
 };
 
 const SignInButton = ({ text }: Props) => {
-	return (
-		<Button onClick={ () => { signIn('google').catch(console.error); } }>
-			{ text }
-		</Button>
-	);
+  return (
+    <Button
+      onClick={() => {
+        signIn("google").catch(console.error);
+      }}
+    >
+      {text}
+    </Button>
+  );
 };
 
 export default SignInButton;
